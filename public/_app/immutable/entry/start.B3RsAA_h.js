09192392e1baa535ea51eb5c8f8f8a7114efccc8
sKuhLight <sKuhLight@users.noreply.github.com>
2026-07-01 17:08:50 +0200

    remote gate: add legal footer (privacy · terms · imprint · copyright)

--- public/_app/immutable/entry/start.B3RsAA_h.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/K2uj10bf.js";export{o as load_css,r as start};
---
+++ public/_app/immutable/entry/start.BcXEI1Ga.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/Cq1Ps0Kw.js";export{o as load_css,r as start};
